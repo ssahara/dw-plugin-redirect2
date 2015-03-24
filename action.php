8d27091953bc260f79a20a2be808d404a3982faf
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-03-24 20:29:40 +0900

    rewrite code entirely

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Redirect2 - DokuWiki Redirect manager
+ * Redirect2 - DokuWiki Redirect Manager
  * 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
@@ -54,7 +54,7 @@ class action_plugin_redirect2 extends DokuWiki_Action_Plugin {
      * Redirection of pages
      */
     function redirectPage(&$event, $param){
-        global $ACT, $ID, $INFO, $INPUT, $conf;
+        global $ACT, $ID, $INPUT;
 
         if (empty($this->pattern)) return;
         if( !($ACT == 'show' || (!is_array($ACT) && substr($ACT, 0, 7) == 'export_')) ) return;
@@ -63,7 +63,7 @@ class action_plugin_redirect2 extends DokuWiki_Action_Plugin {
         if ($INPUT->str('redirect',null) == 'no') return;
 
         /*
-         * Redirect based on simple prefix match of the current pagename
+         * Redirect based on simple prefix match of the current page
          * (Redirect Directives)
          * ページをConfファイルで指定する場合は、":"で始めてはならない。
          * ":"で始まる場合は メディアファイルの指定と見なすため。
@@ -72,64 +72,28 @@ class action_plugin_redirect2 extends DokuWiki_Action_Plugin {
         $checkID = $ID;
         do {
             if (isset($this->pattern[$checkID])) {
-                if (preg_match('/^https?:\/\//', $this->pattern[$checkID]['destination'])) {
-                    $url = $this->pattern[$checkID]['destination'];
-                    // リダイレクト先の末尾が"/"の場合、$leafを付加する。
-                    if (substr($url,-1)=='/') $url.= $leaf;
-                    http_status($this->pattern[$checkID]['status']);
-                    send_redirect($url);
-                } else {
-                    if ($this->getConf('show_msg')) {
-                        $title = hsc(useHeading('navigation') ? p_get_first_heading($ID) : $ID);
-                        $class = ($INFO['exists']) ? 'wikilink1' : 'wikilink2';
-                        msg(sprintf($this->getLang('redirected_from'), '<a href="'.
-                            wl($ID, array('redirect' => 'no'), TRUE, '&').'" rel="nofollow"'.
-                            ' class="'.$class.'" title="'.$title.'">'.$title.'</a>'), 0);
-                    }
-                    list($page, $section) = explode('#', $this->pattern[$checkID]['destination'], 2);
-                    // リダイレクト先の末尾が":"の場合、$leafを付加する。
-                    if (substr($page,-1) == ':') $page.= $leaf;
-                    $url = wl($page, '', true);
-                    if (!empty($section)) $url.= '#'.rawurlencode($section);
-                    http_status($this->pattern[$checkID]['status']);
-                    send_redirect($url);
-                }
+                if ($this->getConf('show_msg')) $this->_show_message();
+                $url = $this->_buildURL( $this->pattern[$checkID]['destination'], $leaf);
+                http_status($this->pattern[$checkID]['status']);
+                send_redirect($url);
                 exit;
             }
-            
-            // check prefix hierarchic namespace replacement
-            // ルート名前空間もリダイレクト可能なはず
+            // check hierarchic namespace replacement
             $checkID = ($checkID == ':') ? false : getNS(rtrim($checkID,':')).':';
         } while ($checkID != false);
 
         /*
-         * Redirect based on a regular expression match against the current pagename
+         * Redirect based on a regular expression match against the current page
          * (RedirectMatch Directives)
          */
-        $checkID = $ID;
-        foreach ($this->pattern as $pattern => $data) {
-            if (preg_match('/^%.*%$/', $pattern) !== 1) continue; // 正規表現以外はスルーする
-            $checkID = preg_replace( $pattern, $data['destination'], $checkID, -1, $count);
-            if ($count > 0) {
-                $status = $data['status'];
-                break;
-            }
+        $redirect = $this->_RedirectMatch($ID);
+        if ($redirect !== false) {
+            if ($this->getConf('show_msg')) $this->_show_message();
+            $url = $this->_buildURL( $redirect['destination'], '');
+            http_status($redirect['status']);
+            send_redirect($url);
+            exit;
         }
-        if ($checkID == $ID) return;
-
-        if ($this->getConf('show_msg')) {
-                        $title = hsc(useHeading('navigation') ? p_get_first_heading($ID) : $ID);
-                        $class = ($INFO['exists']) ? 'wikilink1' : 'wikilink2';
-                        msg(sprintf($this->getLang('redirected_from'), '<a href="'.
-                            wl($ID, array('redirect' => 'no'), TRUE, '&').'" rel="nofollow"'.
-                            ' class="'.$class.'" title="'.$title.'">'.$title.'</a>'), 0);
-        }
-        list($page, $section) = explode('#', $checkID, 2);
-        $url = wl($page, '', true);
-        if (!empty($section)) $url.= '#'.rawurlencode($section);
-        http_status($status);
-        send_redirect($url);
-        exit;
     }
 
 
@@ -153,43 +117,107 @@ class action_plugin_redirect2 extends DokuWiki_Action_Plugin {
 
 
     /*
-     * Media Redirect
+     * Redirect of media
      * FETCH_MEDIA_STATUS event handler
-     * https://www.dokuwiki.org/devel:event:fetch_media_status
+     * @see also https://www.dokuwiki.org/devel:event:fetch_media_status
      *
      * メディアファイルをConfファイルで指定する場合は必ず":"で始めること。
      */
     function redirectMedia(&$event, $param) {
         $checkID = $event->data['media'];
+
+        /*
+         * Redirect based on simple prefix match of the current media
+         * (Redirect Directives)
+         */
         $leaf = noNS($checkID); // end token of the mediaID
+        // for media, $checkID need to be clean with ':' prefixed
         $checkID = (substr($checkID,0,1)!=':') ? ':'.$checkID : $checkID;
         do {
             if (isset($this->pattern[$checkID])) {
-                if (preg_match('/^https?:\/\//', $this->pattern[$checkID]['destination'])) {
-                    $url = $this->pattern[$checkID]['destination'];
-                    // リダイレクト先の末尾が"/"の場合、$leafを付加する。
-                    if (substr($url,-1)=='/') $url.= $leaf;
-                    $event->data['status'] = $this->pattern[$checkID]['status'];
-                    $event->data['statusmessage'] = $url;
-                } else {
-                    $newID = $this->pattern[$checkID]['destination'];
-                    // リダイレクト先の末尾が":"の場合、$leafを付加する。
-                    if (substr($newID,-1) == ':') $newID.= $leaf;
-                    error_log('mediaRedirect: '.$checkID.' ->'. $newID);
-                    $url = ml($newID,'',true);
-                    $event->data['status'] = $this->pattern[$checkID]['status'];
-                    $event->data['statusmessage'] = $url; 
-                }
-                break; // Redirect will happen at lib/exe/fetch.php
+                $url = $this->_buildURL( $this->pattern[$checkID]['destination'], $leaf);
+                $event->data['status'] = $this->pattern[$checkID]['status'];
+                $event->data['statusmessage'] = $url;
+                return; // Redirect will happen at lib/exe/fetch.php
             }
-            // check prefix hierarchic namespace replacement
-            // ルート名前空間のメディア全体は"::"始まりで指定する
+            // check hierarchic namespace replacement
             $checkID = ($checkID == '::') ? false : ':'.getNS(trim($checkID,':')).':';
         } while ($checkID != false);
         
-        // 正規表現ベースでのメディアファイルのリダイレクト
-        // Confファイルでの指定は、ページとメディア共通で良いか？
-        // 少なくとも、":"始まりの検索パターンを指定するのは面倒。
-        // メディアファイルは必ず拡張子がある（ページは拡張子なし）。
+        /*
+         * Redirect based on a regular expression match against the current media
+         * (RedirectMatch Directives)
+         */
+        $redirect = $this->_RedirectMatch($event->data['media']);
+        if ($redirect !== false) {
+            $url = $this->_buildURL( $redirect['destination'],'');
+            $event->data['status'] = $redirect['status'];
+            $event->data['statusmessage'] = $url;
+            // Redirect will happen at lib/exe/fetch.php
+        }
     }
+
+
+    /*
+     * Resolve destination page/media id by regular expression match
+     * using rediraction pattern map config file
+     * @param string $checkID  full and cleaned name of page or media
+     *                         for the page, it must be clean id
+     *                         for media, it must be clean with ':' prefixed
+     * @return array of status and destination (id), or false if no matched
+     */
+    protected function _RedirectMatch( $checkID ) {
+        foreach ($this->pattern as $pattern => $data) {
+            if (preg_match('/^%.*%$/', $pattern) !== 1) continue;
+            $destID = preg_replace( $pattern, $data['destination'], $checkID, -1, $count);
+            if ($count > 0) {
+                $status = $data['status'];
+                return array('status' => $status, 'destination' => $destID);
+                break;
+            }
+        }
+        return false;
+    }
+
+    /*
+     * Show message to inform user redirection
+     *
+     * タイトル取得に page title プラグインを考慮するように改造予定
+     */
+    protected function _show_message() {
+        global $ID, $INFO;
+        $title = hsc(useHeading('navigation') ? p_get_first_heading($ID) : $ID);
+        $class = ($INFO['exists']) ? 'wikilink1' : 'wikilink2';
+        msg(sprintf($this->getLang('redirected_from'), '<a href="'.
+                    wl($ID, array('redirect' => 'no'), TRUE, '&').'" rel="nofollow"'.
+                    ' class="'.$class.'" title="'.$title.'">'.$title.'</a>'), 0);
+    }
+
+    /*
+     * Build URL used in send_redirect()
+     * 
+     * @param string $id   id of the page or media of destination of redirect
+     * @param string $leaf (optional) last token of the original page/media id
+     * @return string      url of the destination page/media
+     */
+    protected function _buildURL( $id, $leaf='') {
+        if (preg_match('|^https?:\/\/|', $id)) {
+            // external url, append $leaf if the url end with "/"
+            $url = $id;
+            if (substr($url,-1)=='/') $url.= $leaf;
+
+        } elseif (strpos($id, '.') === false) {
+            // page, 
+            list($page, $section) = explode('#', $id, 2);
+            if (substr($page,-1) == ':') $page.= $leaf;
+            $url = wl($page, '', true);
+            if (!empty($section)) $url.= '#'.rawurlencode($section);
+        } else {
+            // media, append $leaf if the id end with ":"
+           if (substr($id,-1) == ':') $id.= $leaf;
+           $url = ml($id, '', true);
+        }
+        return $url;
+    }
+
 }
