891de07f536eea7cbb36b558d9be3434a81453c9
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-04-04 14:47:31 +0900

    correct log data table in admin window

--- admin.php
+++ admin.php
@@ -13,16 +13,11 @@ require_once(DOKU_PLUGIN.'admin.php');
 
 class admin_plugin_redirect2 extends DokuWiki_Admin_Plugin {
 
-    protected $ConfFile; // path/to/redirection config file
     protected $LogFile;
-    protected $LogData;
 
     function __construct() {
         global $conf;
-
-        $this->ConfFile = DOKU_CONF.'redirect.conf';
         $this->LogFile = $conf['cachedir'].'/redirection.log';
-        $this->LogData = array();
     }
 
     /**
@@ -45,8 +40,10 @@ class admin_plugin_redirect2 extends DokuWiki_Admin_Plugin {
      */
     function handle() {
         global $INPUT;
+        $map = plugin_load('helper', $this->getPluginName());
+
         if ($_POST['redirdata'] && checkSecurityToken()) {
-            if (io_saveFile($this->ConfFile, cleanText($INPUT->str('redirdata')))) {
+            if (io_saveFile($map->ConfFile, cleanText($INPUT->str('redirdata')))) {
                 msg($this->getLang('saved'), 1);
             }
         }
@@ -57,28 +54,31 @@ class admin_plugin_redirect2 extends DokuWiki_Admin_Plugin {
      */
     function html() {
         global $lang;
+        $map = plugin_load('helper', $this->getPluginName());
+
         echo $this->locale_xhtml('intro');
         echo '<form action="" method="post" >';
         echo '<input type="hidden" name="do" value="admin" />';
         echo '<input type="hidden" name="page" value="'.$this->getPluginName().'" />';
         echo '<input type="hidden" name="sectok" value="'.getSecurityToken().'" />';
         echo '<textarea class="edit" rows="15" cols="80" style="height: 300px" name="redirdata">';
-        echo formtext(io_readFile($this->ConfFile));
+        echo formtext(io_readFile($map->ConfFile));
         echo '</textarea><br />';
         echo '<input type="submit" value="'.$lang['btn_save'].'" class="button" />';
         echo '</form>';
 
         if (!$this->getConf('logging')) return;
-        $this->loadLogData();
+        $logData = $this->getLogData();
 
-        echo '<br />';
+        echo $this->locale_xhtml('loginfo');
         echo '<br />';
         echo '<table class="'.$this->getPluginName().'">';
-        echo '<tr><th>Count</th><th>Page/Media</th><th>Redirect to</th><th>Recent redirection</th></tr>';
-        foreach ($this->LogData as $id => $data) {
+        echo '<tr><th>Count</th><th>Status</th><th>Page/Media</th><th>Redirect to</th><th>Recent redirection</th></tr>';
+        foreach ($logData as $id => $data) {
             echo '<tr>';
             echo '<td>'.$data['count'].'</td>';
-            echo '<td>'.$id.'</td>';
+            echo '<td>'.$data['status'].'</td>';
+            echo '<td>'.hsc($id).'</td>';
             echo '<td>'.urldecode($data['redirect']).'</td>';
             echo '<td>'.$data['last'].'</td>';
             echo '</tr>';
@@ -89,28 +89,36 @@ class admin_plugin_redirect2 extends DokuWiki_Admin_Plugin {
 
 
     /**
-     * Load log file
+     * Load redierction data from log file
      */
-    function loadLogData() {
-        if (!file_exists($this->LogFile)) return;
-        
+    protected function getLogData() {
+        $logData = array();
+        if (!file_exists($this->LogFile)) return $logData;
+
         $logfile = new SplFileObject($this->LogFile);
         $logfile->setFlags(SplFileObject::READ_CSV);
         $logfile->setCsvControl("\t"); // tsv
 
         foreach ($logfile as $line) {
             if ($line[0] == NULL) continue;
-            list($datetime, $id, $url) = $line;
-            if (!isset($this->LogData[$id])) {
-                $this->LogData[$id] = array('count' => 1, 'redirect' => $url, 'last' => $datetime);
+            list($datetime, $status, $id, $url) = $line;
+            if (!isset($logData[$id])) {
+                $logData[$id] = array(
+                        'count'    => 1,
+                        'status'   => $status,
+                        'redirect' => $url,
+                        'last'     => $datetime,
+                );
             } else {
-                $this->LogData[$id]['count'] ++;
-                if ($datetime > $this->LogData[$id]['last']) 
-                    $this->LogData[$id]['last'] = $datetime;
+                $logData[$id]['count'] ++;
+                if ($datetime > $logData[$id]['last']) {
+                    $logData[$id]['last'] = $datetime;
+                }
             }
         }
         unset($logfile);
-        uasort($this->LogData, array($this, 'compareCounts'));
+        uasort($logData, array($this, 'compareCounts'));
+        return $logData;
     }
 
     protected function compareCounts($a, $b) {
